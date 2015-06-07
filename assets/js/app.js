6c0496d252244f946a754bd5968a972a2913f65a
Chris Marshall <chris@chrismar035.com>
2015-06-07 01:09:24 -0400

    Render task list on index.html

---
+++ assets/js/app.js
@@ -0,0 +1,61 @@
+var TaskBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadTasksFromServer();
+    setInterval(this.loadTasksFromServer, this.props.pollInterval);
+  },
+  loadTasksFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+    return (
+      <div className="taskBox">
+        <TaskList data={this.state.data} />
+      </div>
+    );
+  }
+});
+
+var TaskList = React.createClass({
+  render: function() {
+    var taskNodes = this.props.data.map(function (task) {
+      return (
+        <Task key={task.id} name={task.name} />
+      );
+    });
+    return (
+      <div className="taskList">
+        {taskNodes}
+      </div>
+    );
+  }
+});
+
+var Task = React.createClass({
+  render: function() {
+    return (
+      <div className="task">
+        <div className="taskName">
+          {this.props.name}
+        </div>
+      </div>
+    );
+  }
+});
+
+React.render(
+  <TaskBox url="todos" pollInterval={2000} />,
+  document.getElementById('content')
+);
